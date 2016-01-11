f8ce6b077ca0ca0479cf0302be665677607d992b
kevin <liujinsong@dana.com>
2016-01-11 11:54:31 +0800

    add blogs and gitbash

---
+++ Blogs/git tags 使用 远程创建git tags 删除（小抄版） - 夜鸣猪的Ruby On Rails 空间 - ITeye技术网站_files/794_data/s.js
@@ -0,0 +1,20 @@
+ADFHOST3643="http://csdnim.qtmojo.com";
+ADFCID3643=236;
+ADFBID3643=3643;
+ADFLOC='CNGDGZ00';
+ADFUSER3643="http://csdnim.qtmojo.com/c?d=csdnim&i=z236,10483621,3643,18529&rf=http%3A%2F%2Fhlee.iteye.com%2Fiframe_ggbd%2F794&a=dwCurl9KE3QXp0BTeux79fEdJapl&u=";
+ALLYESID4="CVyNv3ram16BHIl77Vy";
+ADFPGC="3,22,236,0,4781,3643,18529,10483621";
+
+document.write("<script type=\"text/javascript\"><!--\n");
+document.write("google_ad_client = \"ca-pub-8990951720398508\";\n");
+document.write("/* Iteye博客内页下方banner */\n");
+document.write("google_ad_slot = \"8267689356/1918544322\";\n");
+document.write("google_ad_width = 468;\n");
+document.write("google_ad_height = 60;\n");
+document.write("//-->\n");
+document.write("</Scr"+"ipt>\n");
+document.write("<script type=\"text/javascript\"\n");
+document.write("src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\">\n");
+document.write("</Scr"+"ipt>");
+
